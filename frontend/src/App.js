d482e5a257f49d5fa427d7dcfd49776171db5bd9
Ulises Barragan <200300681@ucaribe.edu.mx>
2025-03-12 12:20:58 -0500

    configuración inicial del dashboard (frontend)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./pages/dashboard"; // Importamos el Dashboard
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
